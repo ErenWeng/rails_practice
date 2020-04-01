2049d251c206523bb6aed08c03f1c6e9e18f93fc
Eren Weng <f245986@gmail.com>
2020-04-01 17:12:46 +0800

    login logout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,49 @@
 class UsersController < ApplicationController
 
   def login
-    # render html: "hi"
+    @user = User.new
+  end
+
+  def logout
+    session[:ccc9527] = nil
+    redirect_to root_path
+  end
+
+  def sign_in
+    #查資料庫
+    @user = User.find_by(email: user_params[:email],
+                        password: user_params[:password])
+    if @user
+      session[:ccc9527] = @user.email
+      redirect_to root_path
+    else
+      redirect_to sign_in_path
+    end
   end
 
   def sign_up
+    @user = User.new
   end
 
   def registration
-    # render html: params[:user]
-    user = params[:user]
-    
-    if user[:password] == user[:password_confirm]
-      redirect_to "/login"
-      
-    else
-      redirect_to "/sign_up" 
-    end
+    @user = User.new(user_params)
+      if @user.save
+      #成功
+        session[:ccc9527] = @user.email
+      #缺登入
+        redirect_to root_path
+      else
+      #失敗
+        render :sign_up
+        #redirect_to "sign_up"   
+      end 
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email,
+                                 :password,
+                                 :password_confirmation)
   end
 
 end
